15519ddb7b7557f703cbf604d91d6a993bba481c
emre.ayhan <emre.ayhan@kgteknoloji.com>
2019-10-06 22:30:03 +0300

    Updates

---
+++ precache-manifest.749fee0576b47afadb528cf00a5ba932.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc6c60330dcfdaefcf8be2788000ba2",
+    "url": "/scrum-poker/index.html"
+  },
+  {
+    "revision": "b9732e9bf56c4aafbbb8",
+    "url": "/scrum-poker/static/css/main.f1cdf4ac.chunk.css"
+  },
+  {
+    "revision": "c37e9d5f0a95b1639e9b",
+    "url": "/scrum-poker/static/js/2.f5e0156c.chunk.js"
+  },
+  {
+    "revision": "b9732e9bf56c4aafbbb8",
+    "url": "/scrum-poker/static/js/main.a4a00221.chunk.js"
+  },
+  {
+    "revision": "268b8fdc0e0781b566a1",
+    "url": "/scrum-poker/static/js/runtime-main.a43ee38a.js"
+  }
+]);
